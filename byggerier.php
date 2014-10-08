d04c965c9251cc6f7542cb264151da25d4a95cb2
Christian Lank <lank.christian@gmail.com>
2014-10-08 22:06:41 +0200

    Fixed layout on kontakt.php & tegnestuen.php
    Der er nu samme layout på alle subsites

--- byggerier.php
+++ byggerier.php
@@ -3,15 +3,15 @@
 <section id="byggerier">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8"> <!-- Main col-md-8 -->
+			<div class="col-sm-9"> <!-- Main col-md-8 -->
 				<div class="page-header" id="section-testemonials">
 					<h2 id="boligbebyggelser">Boligbebyggelse <small>Gruppebebyggelser og byhuse </small></h2>
 				</div>
 				<div class="row">
-					<div class="col-sm-5 col-sm-offset-1 text-left">
-						<img class="img-responsive" src="http://lorempixel.com/300/200/city/1" alt="#">
+					<div class="col-sm-5 col-sm-offset-1">
+						<img class="img-responsive" src="http://lorempixel.com/400/200/city/1" alt="#">
 					</div>
-					<div class="col-sm-6">
+					<div class="col-sm-6 text-left">
 						<h2>Projektnavn</h2>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, ratione porro doloribus ducimus reprehenderit nobis at voluptates ipsa dicta nostrum.</p>
 						<a href="projekt_01.php" class="btn btn-default">Se mere</a>
@@ -19,7 +19,7 @@
 				</div> <!-- end row -->
 				<div class="row">
 					<div class="col-sm-5 col-sm-offset-1 text-left">
-						<img class="img-responsive" src="http://lorempixel.com/300/200/city/6" alt="#">
+						<img class="img-responsive" src="http://lorempixel.com/400/200/city/6" alt="#">
 					</div>
 					<div class="col-sm-6">
 						<h2>Projektnavn</h2>
@@ -29,7 +29,7 @@
 				</div> <!-- end row -->
 				<div class="row">
 					<div class="col-sm-5 col-sm-offset-1 text-left">
-						<img class="img-responsive" src="http://lorempixel.com/300/200/city/3" alt="#">
+						<img class="img-responsive" src="http://lorempixel.com/400/200/city/3" alt="#">
 					</div>
 					<div class="col-sm-6">
 						<h2>Projektnavn</h2>
@@ -42,7 +42,7 @@
 				</div>
 				<div class="row">
 					<div class="col-sm-5 col-sm-offset-1 text-left">
-						<img class="img-responsive" src="http://lorempixel.com/300/200/city/4" alt="#">
+						<img class="img-responsive" src="http://lorempixel.com/400/200/city/4" alt="#">
 					</div>
 					<div class="col-sm-6">
 						<h2>Projektnavn</h2>
@@ -52,7 +52,7 @@
 				</div> <!-- end row -->
 			</div> <!-- end of main col-md-8-->
 
-			<div class="col-sm-3 col-sm-offset-1"> <!-- sidebar -->
+			<div class="col-sm-2 col-sm-offset-1"> <!-- sidebar -->
 				<div id="sidebar">
 					<ul class="nav sidemenu text-left" data-spy="affix" data-offset-top="90">
 						<li><a href="#boligbebyggelser">Boligbebyggelser</a></li>
--- kontakt.php
+++ kontakt.php
@@ -1,68 +1,89 @@
 <?php include'header.php'; ?>
 
-<section id="contact.php">
+<section id="contact">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-6">
-				<h2>Caspersen &amp; Krogh Arkitekter A/S</h2>
-				<p>
-					Tøjhushavevej 14
-					<br>
-					8900 Randers C.
-					<br>
-					86 43 31 22 
-					<br>
-					tegnestuen@ckarkitekter.dk 
-				</p>
-				<hr>
-				<p>
-					reg. nr. 4710
-					<br>
-					konto nr. 853 0000 126
-					<br>
-					SE / CVR nr. 14 04 98 43
-					<br>
-					A/S  nr. 18 92 40
-				</p>
+			<div class="col-sm-9"> <!-- Main column -->
+				<div class="page-header">
+					<h2>Caspersen &amp; Krogh Arkitekter A/S</h2>
+				</div>
+				<div class="row">
+					<div class="col-sm-5 col-sm-offset-1">
+						<p>
+							Tøjhushavevej 14
+							<br>
+							8900 Randers C.
+							<br>
+							86 43 31 22 
+							<br>
+							tegnestuen@ckarkitekter.dk 
+						</p>
+					</div>
+					<div class="col-sm-6">
+						<p>
+							reg. nr. 4710
+							<br>
+							konto nr. 853 0000 126
+							<br>
+							SE / CVR nr. 14 04 98 43
+							<br>
+							A/S  nr. 18 92 40
+						</p>
+					</div>
+				</div> <!-- End row -->
+				<div class="row">
+					<div class="col-sm-11 col-sm-offset-1">
+						<div id="map-canvas"></div>
+					</div>
+				</div> <!-- End row -->
+				<div class="row">
+					<div class="col-sm-5 col-sm-offset-1 text-left">
+						<img class="img-responsive" src="http://lorempixel.com/400/200/business/2" alt="#">
+					</div>
+				<div class="col-sm-6">
+					<h3>Navn Efternavn</h3>
+					<p>
+						Mobil tlf. 23 42 56 50
+						<br>
+						Fastnet tlf. 87 61 16 21
+						<br>
+						HCC@ckark.dk
+					</p>
+				</div>
+			</div> <!-- End row -->
 
-			</div>
-			<div class="col-sm-6">
-				<div id="map-canvas"></div>
-			</div>
-		</div> <!-- End row -->
+			<div class="row">
+				<div class="col-sm-5 col-sm-offset-1 text-left">
+					<img class="img-responsive" src="http://lorempixel.com/400/200/business/3" alt="#">
+				</div>
+				<div class="col-sm-6">
+					<h3>Navn Efternavn</h3>
+					<p>
+						Mobil tlf. 23 42 56 50
+						<br>
+						Fastnet tlf. 87 61 16 21
+						<br>
+						HCC@ckark.dk
+					</p>
+				</div>
+			</div> <!-- End row -->
 
-		<div class="row">
-			<div class="col-sm-4">
-				<h3>Navn Efternavn</h3>
-				<p>
-					Mobil tlf. 23 42 56 50
-					<br>
-					Fastnet tlf. 87 61 16 21
-					<br>
-					HCC@ckark.dk
-				</p>
-			</div>
-			<div class="col-sm-4">
-				<h3>Navn Efternavn</h3>
-				<p>
-					Mobil tlf. 23 42 56 50
-					<br>
-					Fastnet tlf. 87 61 16 21
-					<br>
-					HCC@ckark.dk
-				</p>
-			</div>
-			<div class="col-sm-4">
-				<h3>Navn Efternavn</h3>
-				<p>
-					Mobil tlf. 23 42 56 50
-					<br>
-					Fastnet tlf. 87 61 16 21
-					<br>
-					HCC@ckark.dk
-				</p>
-			</div>
-		</div> <!-- End row -->
+			<div class="row">
+				<div class="col-sm-5 col-sm-offset-1 text-left">
+					<img class="img-responsive" src="http://lorempixel.com/400/200/business/4" alt="#">
+				</div>
+				<div class="col-sm-6">
+					<h3>Navn Efternavn</h3>
+					<p>
+						Mobil tlf. 23 42 56 50
+						<br>
+						Fastnet tlf. 87 61 16 21
+						<br>
+						HCC@ckark.dk
+					</p>
+				</div>
+			</div> <!-- End row -->
+			</div> <!-- End main column -->
 	</div> <!-- End container -->
 </section>
 
